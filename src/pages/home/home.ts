072db622ef050a308239e26ae3b49aaf4c412414
MobileMaster <mobilemaster128@gmail.com>
2017-11-03 04:28:53 -0700

    android

---
+++ src/pages/home/home.ts
@@ -0,0 +1,43 @@
+import { Component } from "@angular/core";
+import { NavController, Platform } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  constructor(public navCtrl: NavController, public platform: Platform) {}
+
+  ionViewDidLoad() {
+    this.platform.ready().then(() => {
+      TheoremReachPlugin.enableDebugMode(true);
+      TheoremReachPlugin.onReward(function(quantity) {
+        console.log("TheoremReach onReward:" + quantity);
+      });
+
+      TheoremReachPlugin.onRewardCenterOpened(function() {
+        console.log("TheoremReach onRewardCenterOpened in Cordova");
+      });
+
+      TheoremReachPlugin.onRewardCenterClosed(function() {
+        console.log("TheoremReach onRewardCenterClosed in Cordova");
+      });
+
+      TheoremReachPlugin.theoremreachSurveyAvailable(function(surveyAvailable) {
+        console.log("TheoremReach theoremreachSurveyAvailable in Cordova");
+      });
+    });
+  }
+
+  ShowReward() {
+    console.log("clicked");
+    TheoremReachPlugin.isSurveyAvailable(function(result) {
+      if (result) {
+        console.log("available");
+        TheoremReachPlugin.showRewardCenter();
+      } else {
+        console.log("unavailable");
+      }
+    });
+  }
+}
